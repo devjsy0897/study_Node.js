345c6bc8d1943f455485a647e1b9e9bcdba78e02
devjsy0897 <devjsy0897@gmail.com>
2022-02-16 21:38:14 +0900

    db 연결 후 회원가입 포맷 만들고 postman으로 테스트 완료

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jsy:1234@studyboilerplate.vd81f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then( ()=> console.log('MongoDB Connected...'))
@@ -10,6 +18,21 @@ mongoose.connect('mongodb+srv://jsy:1234@studyboilerplate.vd81f.mongodb.net/myFi
 
 app.get('/', (req, res) => {    
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res) => {
+  
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것 들을 데이터 베이스에 넣어준다.
+  
+  const user = new User(req.body) 
+
+  user.save((err, userInfo)=>{
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
