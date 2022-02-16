d77f588b3182069bad5b8eee02b1713f2c8c4f0f
devjsy0897 <devjsy0897@gmail.com>
2022-02-16 20:59:54 +0900

    mongodb 연결 완료

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jsy:1234@studyboilerplate.vd81f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then( ()=> console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {    
   res.send('Hello World!')
 })
