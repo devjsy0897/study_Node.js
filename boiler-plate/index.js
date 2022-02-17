5f17c0e993dc6865ef6c42bf6dfc332e3e7299d1
devjsy0897 <devjsy0897@gmail.com>
2022-02-17 21:45:47 +0900

    비밀정보 보호

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -11,13 +14,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jsy:1234@studyboilerplate.vd81f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then( ()=> console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then( ()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {    
-  res.send('Hello World!')
+  res.send('Hello world!')
 })
 
 app.post('/register',(req,res) => {
