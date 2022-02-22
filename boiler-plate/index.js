b89f375f75cd8a821865c8212d41c408ee47d8c2
devjsy0897 <devjsy0897@gmail.com>
2022-02-22 21:39:14 +0900

    로그아웃 기능 완료

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -4,7 +4,7 @@ const port = 3000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-const {auth} = require("./middleware/auth");
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -23,56 +23,61 @@ app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-app.post('/api/users/register',(req,res) => {
-  
-  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
-  // 그것 들을 데이터 베이스에 넣어준다.
-  
-  const user = new User(req.body) 
+app.post('/api/users/register', (req, res) => {
 
-  user.save((err, userInfo)=>{
-    if(err) return res.json({ success: false, err})
+  //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
+  //그것들을  데이터 베이스에 넣어준다. 
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
     return res.status(200).json({
       success: true
     })
   })
 })
 
+app.post('/api/users/login', (req, res) => {
 
-app.post('/api/users/login',(req,res) => {
-  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({ email: req.body.email}, (err, user) =>{
-    if(!user) {
+  // console.log('ping')
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+
+    // console.log('user', user)
+    if (!user) {
       return res.json({
-        loginSuccess:false,
-        message:"제공된 이메일에 해당하는 유저가 없습니다."
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
       })
     }
-    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
-    user.comparePassword( req.body.password, (err, isMatch) => {
-      if(!isMatch)
-      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."
-    })
-      // 비밀번호까지 맞다면 토큰을 생성하기.
-        user.generateToken((err, user)=>{
-          if(err) return res.status(400).send(err);
-          
-          // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
-            res.cookie("x_auth",user.token)
-            .status(200)
-            .json({ loginSuccess: true, userId : user._id})
-          
-        
-      })  
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // console.log('err',err)
+
+      // console.log('isMatch',isMatch)
+
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
-
 })
 
 
-
-app.get('api/users/auth', auth ,(req,res) => {
-  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authenticationi이 True 라는 말.
+// role 1 어드민    role 2 특정 부서 어드민 
+// role 0 -> 일반유저   role 0이 아니면  관리자 
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -85,6 +90,17 @@ app.get('api/users/auth', auth ,(req,res) => {
   })
 })
 
+app.get('/api/users/logout', auth, (req, res) => {
+  // console.log('req.user', req.user)
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
 
 
 
